91fd295a23e64bb649915e4a7ecc8511abe19cda
mutanim24 <mutanim24@gmail.com>
2023-04-11 23:01:39 +0600

    job details updated

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -3,13 +3,13 @@ import bannerImg from '../../assets/All Images/P3OLGJ1 copy 1.png'
 
 const Banner = () => {
     return (
-        <div className='banner grid md:grid-cols-2 gap-4 flex items-center'>
-            <div className='text-center md:text-left'>
+        <div className='banner flex items-center'>
+            <div className='text-center md:text-left w-6/12'>
                 <h1 className='text-5xl md:text-7xl font-bold'>One Step <br /> Closer To Your <br /><span className='text-cyan-500'>Dream Job </span></h1>
                 <p className='my-5 me-12'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
                 <button className='text-white text-xl rounded bg-gradient-to-r from-cyan-600 to-purple-500 p-1 px-3'>Get Start</button>
             </div>
-            <img className='' src={bannerImg} alt="" />
+            <img className='w-6/12' src={bannerImg} alt="" />
         </div>
     );
 };
--- src/component/JobDetails/JobDetails.jsx
+++ src/component/JobDetails/JobDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import { CurrencyDollarIcon, MapPinIcon, PhoneIcon, EnvelopeIcon, BriefcaseIcon } from '@heroicons/react/24/solid'
 
 const JobDetails = () => {
     const { id } = useParams();
@@ -15,11 +16,38 @@ const JobDetails = () => {
             <div>
                 <h1 className='text-4xl font-bold text-center p-4 md:p-9'>Job Details</h1>
             </div>
-            <div class="grid grid-cols-2">
-                <div class="col-span-1">Column 1</div>
-                <div class="col-span-2">Column 2</div>
+            <div className="grid grid-cols-3 gap-4">
+                <div className='col-span-2 space-y-3'>
+                    <p><span className='font-bold'>Job Description: </span>{job_description}</p>
+                    <p><span className='font-bold'>Job Responsibility: </span>{job_responsibility}</p>
+                    <p><span className='font-bold'>Educational Requirements: </span><br />{educational_requirements}</p>
+                    <p><span className='font-bold'>Experiences: </span><br />{experiences}</p>
+                </div>
+                <div>
+                    <div className='rounded bg-gray-50 space-y-3 p-8'>
+                        <h2 className='font-bold text-2xl'>Job Details</h2>
+                        <hr />
+                        <p>
+                            <CurrencyDollarIcon className="h-5 w-5 text-cyan-600 inline-flex" /> <span className='font-semibold'>Salary: </span> {salary}
+                        </p>
+                        <p>
+                            <BriefcaseIcon className="h-5 w-5 text-cyan-600 inline-flex" /> <span className='font-semibold'>Job Title: </span> {job_title}
+                        </p>
+                        <h2 className='text-1xl font-bold'>Contact Information</h2>
+                        <hr />
+                        <p>
+                            <PhoneIcon className="h-5 w-5 text-cyan-600 inline-flex" /> <span className='font-semibold'>Phone: </span> {contact_information.phone}
+                        </p>
+                        <p>
+                            <EnvelopeIcon className="h-5 w-5 text-cyan-600 inline-flex" /> <span className='font-semibold'>Email: </span> {contact_information.email}
+                        </p>
+                        <p>
+                            <MapPinIcon className="h-5 w-5 text-cyan-600 inline-flex" /> <span className='font-semibold'>Address: </span> {location}
+                        </p>
+                    </div>
+                    <button className='w-full text-white bg-cyan-600 p-2 rounded my-8'>Apply Now</button>
+                </div>
             </div>
-
         </div>
     );
 };
